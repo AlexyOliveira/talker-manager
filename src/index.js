58fd84dcd8d304a69a5faa08159a32a2aedc5ee1
alexyoliveira <cupofteaoficial@gmail.com>
2023-03-14 18:58:55 -0300

    test email and password validations

--- src/index.js
+++ src/index.js
@@ -16,6 +16,33 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
+// midlewares
+
+const validateEmailLogin = (req, res, next) => {
+  const { email } = req.body;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const message = { message: 'O campo "email" é obrigatório' };
+  if (!email) {
+   return res.status(400).json(message);
+  } if (!emailRegex.test(email)) {
+    const message2 = { message: 'O "email" deve ter o formato "email@email.com"' };
+   return res.status(400).json(message2);
+  } 
+    next();
+};
+
+const validatePsswordLogin = (req, res, next) => {
+  const { password } = req.body;
+  const message = { message: 'O campo "password" é obrigatório' };
+  const message2 = { message: 'O "password" deve ter pelo menos 6 caracteres' };
+  if (!password) {
+    return res.status(400).json(message);
+   } if (password.length < 6) {
+    return res.status(400).json(message2);
+   }
+   next();
+};
+
 // rotas
 
 app.get('/talker', async (req, res) => {
@@ -36,7 +63,7 @@ app.get('/talker/:id', async (req, res) => {
   }
   });
 
-  app.post('/login', (req, res) => {
+  app.post('/login', validateEmailLogin, validatePsswordLogin, (req, res) => {
    const token = getTokenByUser();
    return res.status(200).json({ token });
     });
